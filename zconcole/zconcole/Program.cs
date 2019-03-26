bfad14c6e1128d52ab0d74da896822bc16fe406a
George Gilman <ggilman@flyingpiano.com>
2019-03-26 14:37:24 -0700

    cuz

---
+++ zconcole/zconcole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace zconcole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
